ebdb7c56d00106c310518b981c53f1fa4c1fceab
Miheer <ac146842@avcol.school.nz>
2026-03-27 09:40:28 +1300

    more inserts

---
+++ Data/SQL/InsertIntoQuestions.sql
@@ -0,0 +1,15 @@
+INSERT INTO FormQuestions (Questions)
+VALUES 
+('How old are you?'),
+('To donate plasma you must weigh 50 kg or more and be 150 cm or taller. To check if you can donate blood click the button below.'),
+('Have you ever received a blood transfusion in the UK, Republic of Ireland or France after 1st January 1980?'),
+('Have you ever had a stroke, heart attack, or cardiac stent?'),
+('Are you under any health investigations and/or awaiting surgery?'),
+('Do you have cough or cold symptoms?'),
+('Have you had COVID in the last 7 days?'),
+('Have you had the Flu in the last 4 weeks?'),
+('Have you or anyone in your household had diarrhoea and/or vomiting in the last 12 weeks?'),
+('Have you had a tattoo or piercing in the last 3 months?'),
+('Are you currently pregnant or have you given birth recently?'),
+('Are you taking any prescription medications including iron?'),
+('Have you travelled outside of New Zealand in the last 12 months?');
